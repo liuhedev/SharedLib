fd8da95c5b9c79bc0488829eca722c2e0caffcfe
liuhea <he.liu@leediancn.com>
2017-03-23 23:46:39 +0800

    other method implement add recyclerview header

--- app/src/main/java/com/clever/ui/fragment/RcyFragment.java
+++ app/src/main/java/com/clever/ui/fragment/RcyFragment.java
@@ -7,11 +7,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.clever.R;
 import com.clever.ui.adapter.HeaderAdapter;
 import com.clever.ui.adapter.ParentAdapter;
-import com.clever.ui.adapter.ParentHeaderAdapter;
+import com.clever.ui.adapter.RecyclerViewHeader;
 
 import java.util.ArrayList;
 
@@ -34,25 +35,25 @@ public class RcyFragment extends Fragment {
 
     private void initView(View view) {
         RecyclerView parentRcy = (RecyclerView) view.findViewById(R.id.rcy_rcy_parent);
+        parentRcy.setLayoutManager(new LinearLayoutManager(getActivity()));
+        RecyclerViewHeader header = (RecyclerViewHeader) view.findViewById(R.id.recycler_header);
+        FrameLayout fl = (FrameLayout) view.findViewById(R.id.fl_header);
+
         View headerView = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_rcy_header, null);
         RecyclerView headerRcy = (RecyclerView) headerView.findViewById(R.id.rcy_task_child);
+        headerRcy.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        fl.addView(headerView);
+//        RecyclerView headerRcy = (RecyclerView) view.findViewById(R.id.rcy_task_child);
+//        headerRcy.setLayoutManager(new LinearLayoutManager(getActivity()));
+        header.attachTo(parentRcy);
 
         ParentAdapter parentAdapter = new ParentAdapter(getActivity(), getTestData("child", 30));
         HeaderAdapter headerAdapter = new HeaderAdapter(getActivity(), getTestData("header", 4));
 
-        ParentHeaderAdapter parentHeaderAdapter = new ParentHeaderAdapter(parentAdapter);
-
-        parentRcy.setLayoutManager(new LinearLayoutManager(getActivity()));
-        headerRcy.setLayoutManager(new LinearLayoutManager(getActivity()));
-
         headerRcy.setAdapter(headerAdapter);
-        parentHeaderAdapter.addHeaderView(headerView);
-
-        parentRcy.setAdapter(parentHeaderAdapter);
-
-
+        parentRcy.setAdapter(parentAdapter);
     }
-
 
     ArrayList<String> getTestData(String where, int count) {
         ArrayList<String> strings = new ArrayList<>();
