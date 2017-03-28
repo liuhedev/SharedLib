8b78e2c642ecee9d8a0f9d6284ec21d645b5be8f
liuhe <liuhe@juran.com.cn>
2017-03-28 18:43:39 +0800

    android6.0（api23）权限适配测试

--- app/src/main/java/com/clever/ui/fragment/MediaFragment.java
+++ app/src/main/java/com/clever/ui/fragment/MediaFragment.java
@@ -1,19 +1,26 @@
 package com.clever.ui.fragment;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.clever.R;
 import com.clever.ui.activity.ImageViewActivity;
 
 public class MediaFragment extends Fragment implements View.OnClickListener {
 
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 10086;
     private Button mAudioPlayBtn;
     private AppCompatButton mPhotoShowBtn;
 
@@ -50,14 +57,64 @@ public class MediaFragment extends Fragment implements View.OnClickListener {
                 onClickAudioPlay();
                 break;
             case R.id.btn_fragment_media_show_photos:
+                onClickCallPhone();
                 break;
         }
 
+    }
+
+    private void onClickCallPhone() {
+        testCall();
     }
 
     private void onClickAudioPlay() {
 
         startActivity(new Intent(getActivity(), ImageViewActivity.class));
         //    http://sherpapreview-standard.s3.cn-north-1.amazonaws.com.cn/Preview/Public/Beta/18307433.m4a
+    }
+
+    public void testCall() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE);
+        } else {
+            callPhone();
+        }
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                Manifest.permission.CALL_PHONE)) {
+            Toast.makeText(getActivity(), "你都选了不要在弹出选择框，还想咋地", Toast.LENGTH_SHORT).show();
+
+            // Show an expanation to the user *asynchronously* -- don't block
+            // this thread waiting for the user's response! After the user
+            // sees the explanation, try again to request the permission.
+
+        }
+    }
+
+    public void callPhone() {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        Uri data = Uri.parse("tel:" + "10086");
+        intent.setData(data);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        if (requestCode == MY_PERMISSIONS_REQUEST_CALL_PHONE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                callPhone();
+            } else {
+                // Permission Denied
+                Toast.makeText(getActivity(), "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
--- app/src/main/java/com/clever/ui/fragment/RcyFragment.java
+++ app/src/main/java/com/clever/ui/fragment/RcyFragment.java
@@ -2,18 +2,11 @@ package com.clever.ui.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.clever.R;
-import com.clever.ui.adapter.ContentAdapter;
-import com.clever.ui.adapter.HeaderAdapter;
-import com.clever.ui.adapter.RecyclerViewHeader;
-
-import java.util.ArrayList;
 
 public class RcyFragment extends Fragment {
 
@@ -22,7 +15,7 @@ public class RcyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rcy_parent, null);
-        initView(view);
+//        initView(view);
         initData();
 
         return view;
@@ -31,28 +24,28 @@ public class RcyFragment extends Fragment {
     private void initData() {
 
     }
-
-    private void initView(View view) {
-        RecyclerView parentRcy = (RecyclerView) view.findViewById(R.id.rcy_rcy_parent);
-        RecyclerViewHeader header = (RecyclerViewHeader) view.findViewById(R.id.recycler_header);
-        RecyclerView headerRcy = (RecyclerView) view.findViewById(R.id.rcy_task_child);
-
-        parentRcy.setLayoutManager(new LinearLayoutManager(getActivity()));
-        headerRcy.setLayoutManager(new LinearLayoutManager(getActivity()));
-        ContentAdapter contentAdapter = new ContentAdapter(getActivity(), getTestData("content", 30));
-        HeaderAdapter headerAdapter = new HeaderAdapter(getActivity(), getTestData("header", 4));
-        headerRcy.setAdapter(headerAdapter);
-        parentRcy.setAdapter(contentAdapter);
-
-        header.attachTo(parentRcy);
-
-    }
-
-    ArrayList<String> getTestData(String where, int count) {
-        ArrayList<String> strings = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            strings.add(where + "->测试数据->" + i);
-        }
-        return strings;
-    }
+//
+//    private void initView(View view) {
+//        RecyclerView parentRcy = (RecyclerView) view.findViewById(R.id.rcy_rcy_parent);
+//        RecyclerViewHeader header = (RecyclerViewHeader) view.findViewById(R.id.recycler_header);
+//        RecyclerView headerRcy = (RecyclerView) view.findViewById(R.id.rcy_task_child);
+//
+//        parentRcy.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        headerRcy.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        ContentAdapter contentAdapter = new ContentAdapter(getActivity(), getTestData("content", 30));
+//        HeaderAdapter headerAdapter = new HeaderAdapter(getActivity(), getTestData("header", 4));
+//        headerRcy.setAdapter(headerAdapter);
+//        parentRcy.setAdapter(contentAdapter);
+//
+//        header.attachTo(parentRcy);
+//
+//    }
+//
+//    ArrayList<String> getTestData(String where, int count) {
+//        ArrayList<String> strings = new ArrayList<>();
+//        for (int i = 0; i < count; i++) {
+//            strings.add(where + "->测试数据->" + i);
+//        }
+//        return strings;
+//    }
 }
